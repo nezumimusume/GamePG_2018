f9f0d44e1d7dff40c90fd0f8a41106dd0bb1663f
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2019-05-13 10:41:30 +0900

    hogehoge

--- tkEngine2/Sample/Lesson01_Question/Game/Player.cpp
+++ tkEngine2/Sample/Lesson01_Question/Game/Player.cpp
@@ -25,33 +25,25 @@ bool Player::Start()
 void Player::Update()
 {
 	//Question 1 キャラを左右に動かしてみよう。
-	if (Pad(0).IsPress(enButtonRight)) {
-		position.x += 10.0f;
-	}
-	if (Pad(0).IsPress(enButtonLeft)) {
-		position.x -= 10.0f;
-	}
-	//実習課題 キャラを前後に動かしてみよう。	
-	if (Pad(0).IsPress(enButtonDown)) {
-		position.z -= 10.0f;
-	}
-	if (Pad(0).IsPress(enButtonUp)) {
-		position.z += 10.0f;
-	}
-
-	//Question 2 キャラクタをジャンプさせてみよう。
-	if (Pad(0).IsTrigger(enButtonA)) {
-		jumpPower = 20.0f;
-	}
-	//Question 3 から重力を減算する。
-	jumpPower -= 1.0f;
-	//ジャンプ力を座標に加算する。
-	position.y += jumpPower;
+	
+	
+	//実習課題 1  キャラを前後に動かしてみよう。
+	
+	
+	//Question 3 キャラクタをジャンプさせてみよう。
+	
+	
+	//Question 4 重力の影響を与えてみよう。
+	
+	
+	//Y方向の速度を座標に加算する。
+	position.y += ySpeed;
 	//キャラクターのY座標が0より小さくなったら
 	//ジャンプ力を0にして、キャラのY座標も0にする。
 	if (position.y <= 0.0f) {
-		jumpPower = 0.0f;
+		ySpeed = 0.0f;
 		position.y = 0.0f;
 	}
+	
 	skinModelRender->SetPosition(position);
 }
