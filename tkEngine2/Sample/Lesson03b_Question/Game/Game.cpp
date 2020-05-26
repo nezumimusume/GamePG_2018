7b6c941a25476b6c8a26500e6d1a21b476be207c
tsukamura <t>
2020-05-26 12:22:07 +0900

    Lesson3b add

---
+++ tkEngine2/Sample/Lesson03b_Question/Game/Game.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "Game.h"
+#include "Player.h"
+#include "BackGround.h"
+#include "tkEngine/light/tkDirectionLight.h"
+
+Game::Game()
+{
+}
+Game::~Game()
+{
+	DeleteGO(m_player);
+	DeleteGO(m_directionLight);
+}
+bool Game::Start()
+{
+	//ÉJÉÅÉâÇê›íËÅB
+	MainCamera().SetTarget({ 0.0f, 100.0f, 0.0f });
+	MainCamera().SetNear(0.1f);
+	MainCamera().SetFar(3000.0f);
+	MainCamera().SetPosition({ 0.0f, 100.0f, -300.0f });
+	MainCamera().Update();
+
+	m_player = NewGO<Player>(0);
+	m_backGround = NewGO<BackGround>(0);
+	m_directionLight = NewGO<prefab::CDirectionLight>(0);
+	CVector3 lightDir = { 0.0f, -0.707, 0.707 };
+	lightDir.Normalize();
+	m_directionLight->SetDirection(lightDir);
+	m_directionLight->SetColor({ 1.0f, 1.0f, 1.0f, 1.0f } );
+
+	GraphicsEngine().GetShadowMap().SetLightDirection(lightDir);
+	return true;
+}
+void Game::Update()
+{
+}
+void Game::Render(CRenderContext& rc)
+{
+}
