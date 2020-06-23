eb33c5674d6a97960a7baf7fc0b5ebbfeb9e4e94
tsukamura <t>
2020-06-23 19:25:28 +0900

    Shooting05 Qes Up

--- tkEngine2/Sample/Lesson13_Answer/Game/Enemy.h
+++ tkEngine2/Sample/Lesson13_Answer/Game/Enemy.h
@@ -10,8 +10,8 @@ public:
 	// ここからメンバ変数
 	///////////////////////////////
 	prefab::CSkinModelRender*	m_skinModelRender = nullptr;	//スキンモデルレンダー。
-	CVector3 m_position = CVector3::Zero;						//座標。
-	CVector3 m_moveSpeed = CVector3::Zero;						//移動速度。
+	CVector3 m_position ;						//座標。
+	CVector3 m_moveSpeed ;						//移動速度。
 	int m_timer = 0;	//タイマー。
 };
 
