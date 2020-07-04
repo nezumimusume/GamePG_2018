c1a9f949f8f30adbd17435e356078c81e7f732f4
tsukamura <t>
2020-07-04 17:38:02 +0900

    Shooting06 ANS add

---
+++ tkEngine2/Sample/Shooting_06_Answer/Game/BackGround.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "BackGround.h"
+BackGround::BackGround()
+{
+}
+BackGround::~BackGround()
+{
+	//スプライトレンダラーのインスタンスを削除。
+	DeleteGO(m_spriteRender);
+}
+bool BackGround::Start()
+{
+	m_spriteRender = NewGO<prefab::CSpriteRender>(0);
+	m_spriteRender->Init(
+		L"sprite/background.dds",
+		g_camera3D->GetWidth(),  //飛行機の後ろに背景を書くための処理。
+		g_camera3D->GetHeight(), //飛行機の後ろに背景を書くための処理。
+		true
+	);
+	return true;
+}
+
